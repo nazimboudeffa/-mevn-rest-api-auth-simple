0ebb0c9d31f5d1c8160d2268a6bf37970f90b5b6
Nazim Boudeffa <boudeffa@gmail.com>
2022-07-05 22:43:07 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+// Import Routes
+const authRoute = require('./routes/auth');
+
+// Route Middleware
+app.use('/api/user', authRoute);
+
+app.listen(5000, ()=> console.log('Server up!'));
