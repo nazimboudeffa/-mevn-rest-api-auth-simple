cbd209f4b81d5a10503a50d925213979cac3c9c2
Nazim Boudeffa <boudeffa@gmail.com>
2022-07-06 21:49:19 +0200

    Hash password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const {registerValidation, loginValidation} = require('../validation');
 
 
@@ -12,18 +13,37 @@ router.post('/register', async (req, res)=>{
     const emailExist = await User.findOne({email: req.body.email});
     if (emailExist) return res.status(400).send('Email already exists');
 
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     // Create a new user
     const user = new User({
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user: user._id});
     } catch(err){
         res.status(400).send(err);
     }
+})
+
+// Login
+router.post('/login', async (req, res) => {
+    const {error} = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('Email not found');
+
+    // Password correct
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if (!validPass) return res.status(400).send('Invalid password');
+
+    res.send('Logged in!')
 })
 
 module.exports = router;
--- validation.js
+++ validation.js
@@ -13,10 +13,10 @@ const registerValidation = (data) =>{
 // Login validation
 const loginValidation = (data) =>{
     const schema = Joi.object({
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
     });
-    return schema.validate(data, schema);
+    return schema.validate(data);
 }
 
 module.exports.registerValidation = registerValidation;
